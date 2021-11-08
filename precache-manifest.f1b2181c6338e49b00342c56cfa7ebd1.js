2675dd5851ffdd02c02daf226c2206c9ad521a27
tapiwachamb <chambokot@africau.edu>
2021-11-08 03:04:32 +0200

    Updates

---
+++ precache-manifest.f1b2181c6338e49b00342c56cfa7ebd1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d62bc6506a9beceb376edf74b619b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3d9e19d485077a0aa6",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "e2f8c3e9bfbe6c3b72c3",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "e85bad6a9cc36c7f4000",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "97f270fee9679625060f",
+    "url": "/static/js/2.a96536c8.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.a96536c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3d9e19d485077a0aa6",
+    "url": "/static/js/3.fef15fd6.chunk.js"
+  },
+  {
+    "revision": "e2f8c3e9bfbe6c3b72c3",
+    "url": "/static/js/4.9e7bc1c0.chunk.js"
+  },
+  {
+    "revision": "e85bad6a9cc36c7f4000",
+    "url": "/static/js/main.e8761d42.chunk.js"
+  },
+  {
+    "revision": "add769ec4c180a119689",
+    "url": "/static/js/runtime-main.e10afa61.js"
+  },
+  {
+    "revision": "80ac687a950a4855fc144126566d39e3",
+    "url": "/static/media/1024px-AWS_Simple_Icons_AWS_Cloud.svg.80ac687a.png"
+  },
+  {
+    "revision": "691299e76c87c6f88a777ee1530e82d9",
+    "url": "/static/media/1541002205903-removebg-preview.691299e7.png"
+  },
+  {
+    "revision": "234310746c1fdef5264143a478634a51",
+    "url": "/static/media/3a77ca711d86a6e66378480dace2b2f2-removebg-preview.23431074.png"
+  },
+  {
+    "revision": "59208497ac0ada6734516bb2d5173f06",
+    "url": "/static/media/4373121_logo_logos_microsoft_icon.59208497.png"
+  },
+  {
+    "revision": "f85a3f2e341ae642f40a1de5dba4dcf9",
+    "url": "/static/media/4373210_kaggle_logo_logos_icon.f85a3f2e.png"
+  },
+  {
+    "revision": "5dd046c8d0d856aa8f6ecf8398d1146a",
+    "url": "/static/media/Africa-University-Logo.5dd046c8.jpg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5257a2d1c84c721cca39891c0cfd21d7",
+    "url": "/static/media/Daco_4533338.5257a2d1.png"
+  },
+  {
+    "revision": "f183094ef38f36841b3fdcbf4a6fb342",
+    "url": "/static/media/Hillcrest_College_Logo.f183094e.JPG"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "21f52c1f7818351fcce1763a20d59a4b",
+    "url": "/static/media/Nvidia_logo.svg.21f52c1f.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "eed087cf3223870674831e3048fb57b0",
+    "url": "/static/media/undraw_Push_notifications_re_t84m-removebg-preview.eed087cf.png"
+  },
+  {
+    "revision": "d958972de6477f49cd6f334c7929689e",
+    "url": "/static/media/undraw_Segment_analysis_re_ocsl-removebg-preview.d958972d.png"
+  },
+  {
+    "revision": "0d7ea8dae7991e2069450d8114f07c01",
+    "url": "/static/media/undraw_medical_care_movn-removebg-preview.0d7ea8da.png"
+  },
+  {
+    "revision": "c88d7ee75e135c7537956ca46776b041",
+    "url": "/static/media/undraw_science_fqhl-removebg-preview.c88d7ee7.png"
+  },
+  {
+    "revision": "44e9dd0fee30db2d94f38a99206a6755",
+    "url": "/static/media/unnamed.44e9dd0f.png"
+  }
+]);
